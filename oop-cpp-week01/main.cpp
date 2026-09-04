1180b1f7ceb5b2979dc131b71b4281c9751529f8
Vo Ho Thai Khang <25ad11001@hcmute.student.edu.vn>
2026-09-04 09:17:57 +0700

    first commit

--- oop-cpp-week01/main.cpp
+++ oop-cpp-week01/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 
 int main() {
+    std::cout <<"Hello, my name is Khang!" << std::endl;
+    std::cout <<"Welcome to OOP with C++.."<< std::endl;
+
     std::cout << "=====================\n";
     std::cout <<"   MY FRIST C++ APP   \n";
     std::cout << "=====================\n";
     std::cout << "Name: Khang\n";
     std::cout << "Course: OOP with C++\n";
-    std::cout << "I'm ready to learn C++!\n";
+    std::cout << "School: HCMUTE\n \n";
+
+    std::cout <<"My favorite language: C++\n";
+    std::cout <<"My goal: Become a software engineer \n";
+    std::cout <<"My message: The thao la ko ngung bo cuoc \n";
+    std::cout << "=====================\n";
+    std::cout <<"   KEEP CODING !!! \n";
+    std::cout << "=====================\n";
     return 0;
 } 
